14273ee13c98abea884ea5af50de406f1515f98a
matwright <mail@matwright.dev>
2021-05-17 13:25:32 +0200

    noirspeak app barebone

---
+++ lib/data.dart
@@ -0,0 +1,38 @@
+const noirscape_terms = [
+  //guys
+  {'category':'men','name': 'egg','meaning':'a guy, a man','example':'I stepped out of the door and some egg walked right into me!'},
+  {'category':'men','name': 'mug','meaning':'a guy, sometimes used to express naivity','example':'oh, you mean that mug we ran into last night? He sure did buy that crazy story!'},
+  {'category':'men','name': 'bruno','meaning':'a guy, male person','example':'now, listen, which one of you brunos shot the girl?'},
+  {'category':'men','name': 'lug','meaning':'a man, usually one who is badly organized or clumsy','example':'get out of my way you clumsy lug'},
+  {'category':'men','name': 'goose','meaning':'a guy, usually a stupid or silly guy','example':'everthing was fine until this goose started talking'},
+  {'category':'men','name': 'gink','meaning':'a guy, usually an idiot or a loser, a contemptible person','example':'who invited that gink to the party?'},
+
+  {'category':'men','name': 'bread and butter man','meaning':'a salary guy, a man with a regular income','example':'ah, he\'s just a regular bread and butter man'},
+  {'category':'men','name': 'cat','meaning':'a person, male or female','example':'who let that crazy cat out of the bag?'},
+  //dames
+  {'category':'women','name': 'bird','meaning':'a person, either male or female, but more commonly female','example':'Where does this bird come into the story?'},
+  {'category':'women','name': 'babe','meaning':'attractive female, term of affection for a significant other','example':'1. I love you babe. \n2.She was one hot babe!'},
+  {'category':'women','name': 'bim','meaning':'a woman, usually of loose morals','example':'I wouldn\t trust her, she\s a real bim alright','source':'https://www.merriam-webster.com/dictionary/bim'},
+  {'category':'women','name': 'broad','meaning':'a woman, usually a woman one finds attractive','example':'She is one hot broad if ever I saw one!'},
+  {'category':'women','name': 'chippy','meaning':'a female prostitute or young woman of easy virtue','example':'He spends all his time and cash with downtown chippies.'},
+  {'category':'women','name': 'cathouse','meaning':'a brothel','example':'He would visit several bars before a night at the cathouse.'},
+  {'category':'women','name': 'canary','meaning':'a female singer','example':'She was some canary, a voice to die for.'},
+  {'category':'women','name': 'chick','meaning':'a woman, usually in reference to her attractiveness','example':'This chick has all the fellas on their knees.'},
+  {'category':'women','name': 'dish','meaning':'an attractive woman','example':'she\'s a real dish if ever I saw one'},
+  {'category':'women','name': 'dizzy with a dame','meaning':'madly in love','example':'He\'s been on the straight and narrow ever since he got dizzy with a dame'},
+  {'category':'women','name': 'kitten','meaning':'an attractive, often playful, young woman, term of affection for a significant other','example':'You know I\d do anything for you, kitten!'},
+  {'category':'women','name': 'looker','meaning':'an attractive woman','example':'Sally was quite the looker. Men would fight over her.'},
+  {'category':'women','name': 'tomato','meaning':'a good looking woman','example':'Who\'s the tomato sat with Bob? Lucky fella.'},
+  {'category':'women','name': 'moll','meaning':'girlfriend, love interest','example':'She\s Jack\'s moll. He\d kill you if you tried anything.'},
+  {'category':'women','name': 'roundheel','meaning':'prostitute, or a woman of easy virtue','example':'She left him and wound up a roundheel downtown.'},
+  {'category':'women','name': 'skirt','meaning':'a woman, women as object of men\'s sexual pursuit','example':'He spends his nights downtown chasing skirts.'},
+  {'category':'women','name': 'sister','meaning':'a woman, often when being spoken down to','example':'Now you listen here sister...'},
+  {'category':'women','name': 'skate around','meaning':'woman of easy virtue','example':'Before Jim married her, she was just skating around.'},
+  {'category':'women','name': 'torcher','meaning':'torch singer, passionate singer (of love)','example':'Betty was a real torcher, boy could she make a grown man cry with her song.'},
+  {'category':'women','name': 'twist','meaning':'a woman, frequently associated with manipulation or jealousy','example':'That twist he\'d been seeing sabotaged his reputation.'},
+  {'category':'women','name': 'worker','meaning':'a woman who takes a guy for his money','example':'He\d been dating this worker who cleaned him out big time.'},
+
+  //detective
+  {'category':'detective','name': 'stumper','meaning':'an extremely difficult puzzle','example':'The case of the missing jewels was a real stumper for detective Wilson.'},
+
+];
